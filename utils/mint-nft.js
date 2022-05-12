8c75b92592d885399d1feb8aa1095a73a3ab649a
kayos <linaaadil1999@gmail.com>
2022-05-12 08:46:11 +0100

    delete hardhat SC locally and deploy it via remix

---
+++ utils/mint-nft.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const API_URL = env.NEXT_PUBLIC_ALCHEMY_RPC_URL;
+const PRIVATE_KEY = env.METAMASK_PRIVATE_KEY;
+const PUBLIC_KEY = env.METAMASK_PUBLIC_KEY;
+
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
+
+const contract = require("../ABI/NFTToken.json");
+const contractAddress = env.SMART_CONTRACT_ADDRESS;
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+
+async function mintNFT(tokenURI) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+
+  //the transaction
+  const tx = {
+    'from': PUBLIC_KEY,
+    'to': contractAddress,
+    'nonce': nonce,
+    'gas': 500000,
+    'maxPriorityFeePerGas': 2999999987,
+    'data': nftContract.methods.mintNFT(tokenURI).encodeABI()
+  };
+
+  const signedTx = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+  const transactionReceipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+  
+  console.log(`Transaction receipt: ${JSON.stringify(transactionReceipt)}`);
+}
+
+mintNFT(1)
